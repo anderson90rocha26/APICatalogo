41743e90aed21a3bfaaf302a140a3fdc7a749b99
anderson90rocha26 <anderson90rocha26@gmail.com>
2024-05-14 19:09:05 -0300

    Populando as tabelas Categorias e produtos

---
+++ APICatalogo/Migrations/20240514203645_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + "Values('Coca-Cola','Refrigerante de Cola 350ml',3.75,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + "Values('Sanduiche de Atum','Sanduiches naturais',11.14,'atum.jpg',29,now(),2)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + "Values('Pudim 100g','doces',4.99,'pudim.jpg',42,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
