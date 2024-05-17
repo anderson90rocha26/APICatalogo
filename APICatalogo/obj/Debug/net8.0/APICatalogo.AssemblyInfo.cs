abadfcd244c1d65f6007f684d03f9ed6f8e94fec
anderson90rocha26 <anderson90rocha26@gmail.com>
2024-05-17 14:58:58 -0300

    Adicionar funcao  ActionResult

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41743e90aed21a3bfaaf302a140a3fdc7a749b99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8d260110393882ecbdb67b058e0a913bf533d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
