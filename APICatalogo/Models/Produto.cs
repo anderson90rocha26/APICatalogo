55eda26da1da50c8e4ffb0c5f9afe73c1053eaf6
anderson90rocha26 <anderson90rocha26@gmail.com>
2024-06-17 16:40:20 -0300

    Personalizando validação de resultado

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,17 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage ="O nome é obrigatório!")]
-    [StringLength(20, ErrorMessage = "O nome deve ter entre 5 a 20 caracters", MinimumLength = 5)]
+    [StringLength(80, ErrorMessage = "O nome deve ter entre 5 a 20 caracters", MinimumLength = 5)]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
@@ -33,4 +35,26 @@ public class Produto
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra do produto deve ser maiúscula!", new[]
+                    {
+                        nameof(this.Nome)
+                    });
+            }
+        }
+        
+        if(this.Estoque <= 0)
+        {
+            yield return new ValidationResult("O estoque deve ser maior que zero!", new[]
+                    {
+                        nameof(this.Estoque)
+                    });
+        }
+    }
 }
